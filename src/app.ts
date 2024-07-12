85f01432056d38ae8985591f404a09b02d830ee2
SepenuSelasie <developerseps5@gmail.com>
2024-07-12 14:35:08 +0000

    Deployment fix

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,7 @@ app.use(json());
 app.use('/api/url', urlRouter);
 
 const PORT = process.env.PORT || 5000;
-app.get("/",(req, res) =>
-{
-    res.send("API has been served");
-}
-)
+
 
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
